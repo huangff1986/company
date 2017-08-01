db4d4128085474a2973657ecfec6c92113b4f71c
huangff1986 <906032172@qq.com>
2017-08-01 17:10:13 +0800

    新增 route

--- admin1/src/app.jsx
+++ admin1/src/app.jsx
@@ -1,1 +1,21 @@
-app.jsx
+import React, { Component, PropTypes } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
+import route from './router/route';
+
+import 'babel-polyfill';
+
+import './style/common.less';
+
+store.subscribe(() => {
+
+});
+
+// 创建根组件
+render(
+	<Provider store={store}>
+		{route}
+	</Provider>,
+	document.body.appendChild(document.createElement('div'))
+);
---
+++ admin1/src/router/route.jsx
@@ -0,0 +1,61 @@
+import React, {Component, PropTypes} from 'react';
+import { Router, Route, Redirect, IndexRoute, browserHistory, hashHistory } from 'react-router';
+import Config from '../config/index';
+import layout from '../component/layout/layout'; // 布局界面
+
+import login from '../containers/login/login';
+
+class Roots extends Component {
+	render() {
+		// 这个组件是一个包裹组件，所有的路由跳转的页面都会以this.props.children的形式加载到本组件下
+		return (
+			<div>{this.props.children}</div>
+		);
+	}
+}
+
+// 模块的按需加载
+// 页面需要访问的时候再加载
+// require.ensure( [依赖模块]， 需要按需加载的callback, webpack为模块打包后的chunk文件名  )
+
+// 首页
+const home = (locationm cb) => {
+	require.ensure([], require => {
+		cb(null, require('../containers/home/homeIndex').default)
+	}, 'home');
+}
+
+// 新闻管理
+const news = ( location, cb ) => {
+	require.ensure([], require => {
+		cb(null, require('../containers/news/lines').default)
+	}, 'news')
+}
+
+// 用户管理
+const user = ( location, cb ) => {
+	require.ensure([], require => {
+		cb(null, require('../containers/user/userIndex').default)
+	}, 'user')
+}
+
+// 登录验证
+const requireAuth = (nextState, replace) => {
+	let token = (new Date()).getTime() - Config.localItem('USER_AUTHORIZATION');
+	if(token > 7200000) {
+		replace({
+			pathname: '/login',
+			state: { nextPathname: nextState.location.pathname }
+		});
+	}
+}
+
+const RouteConfig = (
+	<Router history={browserHistory}>
+		<Route path="/home" component={layout} onEnter={requireAuth}>
+			<IndexRoute path="/home" getComponent={home} onEnter={requireAuth} />
+		</Route>
+	</Router>
+);
+
+export default RouteConfig;
